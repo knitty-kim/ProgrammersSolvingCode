de43451265fa3c2d3ed12e607d4b83a8bdaadcb9
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2022-12-16 16:01:22 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        		int sum = 0;
+		
+		for(int i=2; i<=n; i+=2) {
+			if(i%2==0) sum+=i;
+		}
+		
+		
+		int answer = sum;
+        return answer;
+    }
+}

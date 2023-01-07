88fe42f8b70a1bd74963a4588483f163d05001be
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2023-01-07 18:00:44 +0900

    [level 2] Title: 전화번호 목록, Time: 193.79 ms, Memory: 135 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,22 @@
+import java.util.Map;
+import java.util.HashMap;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        
+        Map<String, Boolean> map = new HashMap<>();
+		
+		for(String str : phone_book) {
+			map.put(str, true);
+		}
+
+		
+		for(int i=0; i<phone_book.length; i++) {
+			for(int j=0; j<phone_book[i].length(); j++) {
+				if(map.containsKey(phone_book[i].substring(0, j))) {
+					return false;
+				}
+			}
+		}
+        return true;
+    }
+}

a04c7a56edf8355c983d3472bfaa2915c9f4c84e
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2023-03-19 20:57:18 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 24.80 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,15 +2,22 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        ArrayList<Integer> answerList = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<>();
 
-        int value = -1;
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] != value) {
-                answerList.add(arr[i]);
-                value = arr[i];
+        list.add(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                list.add(arr[i]);
             }
         }
-        return answerList.stream().mapToInt(i->i).toArray();
+
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
     }
 }

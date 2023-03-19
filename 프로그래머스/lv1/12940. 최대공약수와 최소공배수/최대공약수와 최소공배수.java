ed6ceadf8de925395b9483049f1e114a7d5f91fb
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2023-03-19 16:49:14 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,46 +1,16 @@
-import java.util.*;
 class Solution {
     public int[] solution(int n, int m) {
-        HashMap<Integer, Integer> primeFactorsN = primeFactors(n);
-        HashMap<Integer, Integer> primeFactorsM = primeFactors(m);
-
-        int gcd = 1;
-        int lcm = 1;
-
-        for (Map.Entry<Integer, Integer> entry : primeFactorsN.entrySet()) {
-            int prime = entry.getKey();
-            int powerN = entry.getValue();
-            int powerM = primeFactorsM.getOrDefault(prime, 0);
-
-            gcd *= Math.pow(prime, Math.min(powerN, powerM));
-            lcm *= Math.pow(prime, Math.max(powerN, powerM));
-        }
-
-        for (Map.Entry<Integer, Integer> entry : primeFactorsM.entrySet()) {
-            int prime = entry.getKey();
-            if (!primeFactorsN.containsKey(prime)) {
-                int powerM = entry.getValue();
-                lcm *= Math.pow(prime, powerM);
-            }
-        }
-
+        int gcd = gcd(n, m);
+        int lcm = (n * m) / gcd;
         return new int[]{gcd, lcm};
     }
 
-    public static HashMap<Integer, Integer> primeFactors(int num) {
-        HashMap<Integer, Integer> factors = new HashMap<>();
-
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            while (num % i == 0) {
-                factors.put(i, factors.getOrDefault(i, 0) + 1);
-                num /= i;
-            }
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
         }
-
-        if (num > 1) {
-            factors.put(num, 1);
-        }
-
-        return factors;
+        return a;
     }
 }

9ce7a9c9a8214e50e05edf924634ef36479fcc1b
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2023-03-20 14:54:35 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 27.93 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,18 +2,23 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        LinkedList<Integer> linkedList = new LinkedList<>();
-
-        for (int num : arr) {
-            if (linkedList.isEmpty() || linkedList.peekLast() != num) {
-                linkedList.add(num);
+        //List와 ArrayList 구조 사용
+        List<Integer> list = new ArrayList<>();
+        
+        //list에 arr의 첫 번째 값 대입
+        list.add(arr[0]);
+        for (int i=1; i<arr.length;i++) {
+            //arr의 원소가 중복되지 않는 경우, list에 원소 대입
+            if (arr[i-1] != arr[i]) {
+                list.add(arr[i]);
             }
         }
-
-        int[] answer = new int[linkedList.size()];
-
-        for (int i=0; i<answer.length; i++) {
-            answer[i] = linkedList.poll();
+        
+        int[] answer = new int[list.size()];
+        
+        //list -> int[]로의 변환
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
         }
 
         return answer;

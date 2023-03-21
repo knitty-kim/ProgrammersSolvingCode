2461f38a85bcf51eb9dbea700090d5aec37d6975
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2023-03-21 20:11:33 +0900

    [level 1] Title: 3진법 뒤집기, Time: 10.76 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,11 +1,20 @@
 class Solution {
     public int solution(int n) {
-        //10진법 -> 3진법
-        String ternary = Integer.toString(n, 3);
-        //StringBuilder를 이용한 역순 정렬 및 String 변환
-        String reversedTernary = new StringBuilder(ternary).reverse().toString();
-        //3진법 -> 10진법
-        int answer = Integer.parseInt(reversedTernary, 3);
-        return answer;
+        return convertToDecimal(reverseTernary(n, ""), 0);
+    }
+
+    public static String reverseTernary(int n, String reversed) {
+        if (n == 0) {
+            return reversed;
+        }
+        return reverseTernary(n / 3, n % 3 + reversed);
+    }
+
+    public static int convertToDecimal(String reversedTernary, int index) {
+        if (index == reversedTernary.length()) {
+            return 0;
+        }
+        int currentDigit = reversedTernary.charAt(index) - '0';
+        return currentDigit * (int) Math.pow(3, index) + convertToDecimal(reversedTernary, index + 1);
     }
 }

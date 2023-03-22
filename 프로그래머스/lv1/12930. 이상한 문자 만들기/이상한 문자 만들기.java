6ada911e854196a158ee3371ef830fb3693739df
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2023-03-23 00:26:20 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.64 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,13 +1,30 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        int cnt = 0;
-        String[] array = s.split("");
+       Pattern pattern = Pattern.compile("\\S+");
+        Matcher matcher = pattern.matcher(s);
+        StringBuffer result = new StringBuffer();
 
-        for(String ss : array) {
-            cnt = ss.contains(" ") ? 0 : cnt + 1;
-            answer += cnt%2 == 0 ? ss.toLowerCase() : ss.toUpperCase(); 
+        while (matcher.find()) {
+            String word = matcher.group();
+            StringBuilder transformedWord = new StringBuilder();
+
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+
+                if (i % 2 == 0) {
+                    transformedWord.append(Character.toUpperCase(ch));
+                } else {
+                    transformedWord.append(Character.toLowerCase(ch));
+                }
+            }
+
+            matcher.appendReplacement(result, transformedWord.toString());
         }
-      return answer;
-  }
+
+        matcher.appendTail(result);
+
+        return result.toString();
+    }
 }

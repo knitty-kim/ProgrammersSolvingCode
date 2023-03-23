a22a702ec7fc365ca0123d6987f7d243a46b2d99
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2023-03-23 22:47:52 +0900

    [level 1] Title: 시저 암호, Time: 1.92 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isLowerCase(ch)) {
+                ch = (char) ((ch - 'a' + n) % 26 + 'a');
+            } else if (Character.isUpperCase(ch)) {
+                ch = (char) ((ch - 'A' + n) % 26 + 'A');
+            }
+
+            result.append(ch);
+        }
+
+        return result.toString();
+    }
+}

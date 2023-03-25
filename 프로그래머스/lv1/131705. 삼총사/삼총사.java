af60c99fe92d6f251d0ffc3a48df6c2d3f37978b
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2023-03-25 21:46:21 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -1,6 +1,6 @@
 class Solution {
-    public int solution(int[] number) {
-       int result = 0;
+    public static int solution(int[] number) {
+     int result = 0;
         for (int i = 0; i < number.length; i++) {
             for (int j = i+1; j < number.length; j++) {
                 for (int k = j+1; k < number.length; k++) {

08df439c77f7e5567bdaf662bfd16bf047363e10
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-03-19 14:08:00 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,7 +1,2 @@
 def solution(array):
-    answer = []
-    answer.append(max(array))
-    for i in range(len(array)):
-        if answer[0] == array[i]:
-            answer.append(i)
-    return answer
+    return [max(array), array.index(max(array))]

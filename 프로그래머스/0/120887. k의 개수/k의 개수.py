ce0c85c7bc8181cb0d37243ad00ee8ec71150ff4
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-03-19 22:47:42 +0900

    [level 0] Title: k의 개수, Time: 17.12 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -1,3 +1,2 @@
 def solution(i, j, k):
-    li = list(str(x) for x in range(i, j+1))
-    return sum(y.count(str(k)) for y in li if y.find(str(k)) != -1)
+    return sum(str(i).count(str(k)) for i in range(i, j+1))

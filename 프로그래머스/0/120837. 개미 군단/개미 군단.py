bcf83929842512d0ecb234a856be9cc8a6472047
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-03-19 13:43:06 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -1,11 +1,2 @@
 def solution(hp):
-    answer = 0
-    n, r = divmod(hp, 5)
-    answer += n
-    hp = r
-    n, r = divmod(hp, 3)
-    answer += n
-    hp = r
-    n = hp // 1
-    answer += n
-    return answer
+    return hp // 5 + ((hp % 5) // 3) + ((hp % 5) % 3)

71f3cc22004e25641b307faab4bd40157aa64cf4
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-03-19 18:27:49 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/README.md
+++ 프로그래머스/0/120846. 합성수 찾기/README.md
@@ -1,6 +1,6 @@
 # [level 0] 합성수 찾기 - 120846 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120846?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120846) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 19일 18:20:00
+2024년 03월 19일 18:27:49
 
 ### 문제 설명
 
--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,10 +1,2 @@
 def solution(n):
-    answer = 0
-    for i in range(1, n+1):
-        count = 0
-        for j in range(1, i+1):
-            if i % j == 0:
-                count += 1
-        if count >= 3:
-            answer += 1
-    return answer
+    return len([i for i in range(4, n+1) if not all(i % j for j in range(2, int(i ** 0.5)+1))])

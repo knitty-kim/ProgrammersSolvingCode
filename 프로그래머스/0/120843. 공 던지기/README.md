066ca9e594a86dc037ffb35b5bd383316e1f59d0
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-03-20 22:14:51 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/README.md
+++ 프로그래머스/0/120843. 공 던지기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 공 던지기 - 120843 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120843?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120843) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 20일 22:05:00
+2024년 03월 20일 22:14:53
 
 ### 문제 설명
 
--- 프로그래머스/0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.py
@@ -1,13 +1,6 @@
 def solution(numbers, k):
-    n = len(numbers)
-    if n % 2 == 0:
-        arr = [numbers[i] for i in range(len(numbers)) if i % 2 == 0]
-        while k > len(arr):
-            k %= len(arr)
-        return arr[k-1]
-    else:
-        numbers.extend(numbers)
-        arr2 = [numbers[i] for i in range(len(numbers)) if i % 2 == 0]
-        while k > len(arr2):
-            k %= len(arr2)
-        return arr2[k - 1]
+    numbers.extend(numbers)
+    arr = [numbers[i] for i in range(len(numbers)) if i % 2 == 0]
+    while k > len(arr):
+        k %= len(arr)
+    return arr[k - 1]

3fbf96d9d914235adbef1e5eef4914d664c2ff07
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-03-20 14:59:41 +0900

    [level 0] Title: 가까운 수, Time: 0.00 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,7 +1,3 @@
 def solution(array, n):
-    arr = list(map(lambda i: abs(n-i), array))
-    # min_idx = arr.index(min(arr))
-    idx_arr = list(filter(lambda i: arr[i] == min(arr), range(len(arr))))
-    # print(idx_arr)
-    # print(min(array[i] for i in idx_arr))
-    return min(array[i] for i in idx_arr)
+    array.sort(key=lambda x: (abs(x-n), x-n))
+    return array[0]

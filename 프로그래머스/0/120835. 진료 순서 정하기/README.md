4767498f24eca2ea7e5fab088ebbe1b28cdfb96f
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-03-20 16:41:17 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/README.md
+++ 프로그래머스/0/120835. 진료 순서 정하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 진료 순서 정하기 - 120835 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120835?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120835) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.01 ms
+메모리: 10 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 20일 16:05:20
+2024년 03월 20일 16:41:18
 
 ### 문제 설명
 
--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,11 +1,6 @@
 def solution(emergency):
-    # 배열을 내림차순으로 정렬
-    li = sorted(emergency, reverse=True)
-
-    # 정렬된 배열의 인덱스 + 1 = 응급도 순위
-    # 원래 배열의 순서에 맞게 응급도 순위 매칭
-    # 매칭된 배열을 return
-    answer = [0] * len(li)
-    for i in range(len(li)):
-        answer[emergency.index(li[i])] = i + 1
+    answer = []
+    dic = {e: i+1 for i, e in enumerate(sorted(emergency, reverse=True))}
+    for e in emergency:
+        answer.append(dic[e])
     return answer

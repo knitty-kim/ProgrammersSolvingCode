aceb9b06386caa2f2c9720d7a966407c124ee871
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-03-20 22:19:13 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.py
@@ -1,6 +1,5 @@
 def solution(numbers, k):
     numbers.extend(numbers)
     arr = [numbers[i] for i in range(len(numbers)) if i % 2 == 0]
-    while k > len(arr):
-        k %= len(arr)
+    k %= len(arr)
     return arr[k - 1]

ffa78d2b614c606783390dbbdca6efa3f335d71e
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-03-21 15:24:13 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,15 +1,12 @@
 def solution(id_pw, db):
-    tmp_dic = dict(i for i in db)
-    # print(tmp_dic)
+    # tmp_dic = dict(i for i in db)
+    tmp_dic = dict(db)
 
-    for i in tmp_dic:
+    for _ in tmp_dic:
         if id_pw[0] not in tmp_dic:
-            # print('fail')
             return "fail"
         else:
             if id_pw[1] == tmp_dic[id_pw[0]]:
                 return "login"
-                # print('login')
             else:
-                # print('wrong pw')
                 return "wrong pw"

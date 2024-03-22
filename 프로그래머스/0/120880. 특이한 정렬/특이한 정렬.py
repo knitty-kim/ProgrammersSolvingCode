315ad5bc8910d06a766deab5500dddcb77534697
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-03-22 14:30:12 +0900

    [level 0] Title: 특이한 정렬, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,9 +1,2 @@
 def solution(numlist, n):
-
-    dic = {v: v-n for i, v in enumerate(numlist)}
-    result = sorted(dic.items(), key=lambda i: abs(i[1]))
-    for i in range(len(result)):
-        if abs(result[i-1][1]) == abs(result[i][1]) and result[i-1][1] < 0:
-            result[i], result[i-1] = result[i-1], result[i]
-    answer = [item[0] for item in result]
-    return answer
+    return sorted(numlist, key=lambda x: (abs(x-n), n-x))

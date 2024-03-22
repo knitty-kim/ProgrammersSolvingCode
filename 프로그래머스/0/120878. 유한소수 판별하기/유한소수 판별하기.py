c27fed273c1de7148f7d393d387e558019a7e813
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-03-22 12:00:34 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,20 +1,8 @@
 import math
 def solution(a, b):
-    # 최대공약수 c
-    c = math.gcd(a, b)
-    while a % c == 0 and b % c == 0 and c != 1:
-        a //= c
-        b //= c
-
-    while True:
-        if b % 2 == 0:
-            b //= 2
-        elif b % 5 == 0:
-            b //= 5
-        else:
-            break
-
-    if b != 1:
-        return 2
-    else:
-        return 1
+    b //= math.gcd(a, b)
+    while b % 2 == 0:
+        b //= 2
+    while b % 5 == 0:
+        b //= 5
+    return 1 if b == 1 else 2

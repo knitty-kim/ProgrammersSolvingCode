fb990479b5b03b1c811c393a334f13b42ea17bf7
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-03-22 12:53:33 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,10 +1,6 @@
 def solution(A, B):
-
-    # 최대 (A 길이 - 1)만큼 회전
     for i in range(len(A)):
         if A == B:
             return i
-        a = A[len(A)-1]
-        A = a + A[:len(A)-1]
-        print(A)
+        A = A[-1] + A[:-1]
     return -1

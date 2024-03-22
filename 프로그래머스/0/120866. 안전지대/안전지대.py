cad1ba8adcb5eb36c04964812dc4ff34d0da0a1c
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-03-22 22:44:02 +0900

    [level 0] Title: 안전지대, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -0,0 +1,22 @@
+def solution(board):
+    map_dic = {(i, j): 0 for i in range(len(board)) for j in range(len(board))}
+
+    for row_idx in range(len(board)):
+        for col_idx in range(len(board[row_idx])):
+            if board[row_idx][col_idx] == 1:
+                map_dic[(row_idx, col_idx)] = 1
+                map_dic[(row_idx-1, col_idx)] = 1
+                map_dic[(row_idx+1, col_idx)] = 1
+                map_dic[(row_idx, col_idx-1)] = 1
+                map_dic[(row_idx, col_idx+1)] = 1
+                map_dic[(row_idx-1, col_idx-1)] = 1
+                map_dic[(row_idx-1, col_idx+1)] = 1
+                map_dic[(row_idx+1, col_idx-1)] = 1
+                map_dic[(row_idx+1, col_idx+1)] = 1
+
+    count = 0
+    for i in map_dic.keys():
+        if map_dic[i] == 0:
+            count += 1
+
+    return count

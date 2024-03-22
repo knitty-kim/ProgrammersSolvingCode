6ab2f1e06ac4b9fd6e1936736839a5cc0a51d8a0
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-03-22 21:27:09 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,3 +1,2 @@
 def solution(num, total):
-    a1 = int(total // num - (num - 1) // 2)
-    return [i for i in range(a1, a1+num)]
+    return [i for i in range(int(total // num - (num - 1) // 2), int(total // num - (num - 1) // 2)+num)]

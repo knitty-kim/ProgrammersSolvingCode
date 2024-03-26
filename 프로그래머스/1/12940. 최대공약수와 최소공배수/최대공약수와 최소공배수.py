b2f1889f6c9371d93458feec7bb3462611427cfc
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-03-27 00:53:06 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,4 +1,4 @@
 def solution(n, m):
     gcnum = max(i for i in range(1, min(n, m)+1) if n%i == 0 and m%i ==0)
-    lcnum = min(i for i in range(gcnum, n*m+1) if i%n == 0 and i%m == 0)
-    return [gcnum, lcnum]
+    # lcnum = min(i for i in range(min(n, m), n*m+1) if i%n == 0 and i%m == 0)
+    return [gcnum, int(n*m/gcnum)]

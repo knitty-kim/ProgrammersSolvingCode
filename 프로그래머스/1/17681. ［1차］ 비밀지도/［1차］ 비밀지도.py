10fe5a66466e98e0214f256c0ae7674362126901
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-04-02 14:17:16 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,15 +1,9 @@
 def solution(n, arr1, arr2):
-    answer = list(list(format(i, 'b').zfill(n)) for i in arr1)
-    arr2 = list(list(format(i, 'b').zfill(n)) for i in arr2)
-
-    for i in range(len(arr2)):
-        for j in range(len(arr2[i])):
-            if arr2[i][j] == '1':
-                answer[i][j] = '1'
-
-    for i in range(len(answer)):
-        answer[i] = ''.join(answer[i])
-        answer[i] = answer[i].replace('1', '#')
-        answer[i] = answer[i].replace('0', ' ')
-
+    answer = []
+    for i, j in zip(arr1, arr2):
+        h = bin(i | j)[2:]
+        h = h.zfill(n)
+        h = h.replace('1', '#')
+        h = h.replace('0', ' ')
+        answer.append(h)
     return answer

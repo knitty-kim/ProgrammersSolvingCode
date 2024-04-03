4fb74a8eed3d2ca2b5af51186ea369d1c9dd85f3
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-04-04 00:11:34 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,13 +1,10 @@
 from itertools import combinations
 def solution(nums):
     count = 0
-    li = list(sum(i) for i in combinations(nums, 3))
-    for i in li:
-        flag = True
-        for j in range(2, int(i**0.5)+1):
+    for i in list(sum(num) for num in combinations(nums, 3)):
+        for j in range(2, int(i ** 0.5) + 1):
             if i % j == 0:
-                flag = False
                 break
-        if flag:
+        else:
             count += 1
     return count

4b67226a5a709d6b9e3e45c7586a7cf674487d94
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-04-03 21:23:40 +0900

    [level 1] Title: 모의고사, Time: 2.37 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,14 +1,15 @@
 def solution(answers):
-    pattern1 = [1, 2, 3, 4, 5]
-    pattern2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    cnt = [0, 0, 0]
+    person1 = [1, 2, 3, 4, 5] * (len(answers)//5 + 5)
+    person2 = [2, 1, 2, 3, 2, 4, 2, 5] * (len(answers)//8 + 8)
+    person3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * (len(answers)//10 + 10)
 
-    for i,v in enumerate(answers):
-        if v == pattern1[i%len(pattern1)]:
+    cnt = [0, 0, 0]
+    for i in range(len(answers)):
+        if answers[i] == person1[i]:
             cnt[0] += 1
-        if v == pattern2[i%len(pattern2)]:
+        if answers[i] == person2[i]:
             cnt[1] += 1
-        if v == pattern3[i%len(pattern3)]:
+        if answers[i] == person3[i]:
             cnt[2] += 1
-    return [i + 1 for i, v in enumerate(cnt) if v == max(cnt)]
+    best = max(cnt)
+    return [i+1 for i, v in enumerate(cnt) if v == best]

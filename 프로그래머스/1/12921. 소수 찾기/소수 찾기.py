4c86f63ce75d2270bd547037035d2f2ad92605b8
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-04-04 00:28:52 +0900

    [level 1] Title: 소수 찾기, Time: 245.79 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,9 +1,6 @@
 def solution(n):
-    count = 0
-    for i in range(2, n+1):
-        for j in range(2, int(i**0.5)+1):
-            if i % j == 0:
-                break
-        else:
-            count += 1
-    return count
+    num = set(range(2, n+1))
+    for i in range(2, int(n**0.5)+1):
+        if i in num:
+            num -= set(range(i*i, n+1, i))
+    return len(num)

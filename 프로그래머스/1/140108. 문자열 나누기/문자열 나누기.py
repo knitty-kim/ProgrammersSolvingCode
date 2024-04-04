730d9700923179cdd8a0a3f89d452c1ab039fada
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-04-04 21:30:16 +0900

    [level 1] Title: 문자열 나누기, Time: 1.87 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,21 +1,12 @@
 def solution(s):
     count = [0, 0]
-    slice_count = 0
-    flag = True
+    slice = 0
     for i in range(len(s)):
-        if flag:
+        if count[0] == count[1]:
+            slice += 1
             vari = s[i]
-            flag = False
-
         if vari == s[i]:
             count[0] += 1
         else:
             count[1] += 1
-
-        if count[0] == count[1]:
-            slice_count += 1
-            flag = True
-        else:
-            if i == len(s)-1:
-                slice_count += 1
-    return slice_count
+    return slice

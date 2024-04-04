51cc6aed1e9f70321acde92fbf499991b6a30102
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-04-04 17:02:32 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,37 +1,15 @@
 def solution(dartResult):
-    zone = ['S', 'D', 'T']
-    zone_opt = []
-    for i, v in enumerate(dartResult):
-        if v.isalpha():
-            if dartResult[i+1:i+2] == '*':
-                zone_opt.append((v, '*'))
-            elif dartResult[i+1:i+2] == '#':
-                zone_opt.append((v, '#'))
-            else:
-                zone_opt.append((v, ''))
+    dart = {'S': 1, 'D': 2, 'T': 3}
+    scores = []
+    n = 0
+    for i, d in enumerate(dartResult):
+        if d in dart:
+            scores.append(int(dartResult[n:i])**dart[d])
+        elif d == "*":
+            scores[-2:] = [x*2 for x in scores[-2:]]
+        elif d == "#":
+            scores[-1] = (-1)*scores[-1]
+        if not d.isnumeric():
+            n = i+1
 
-    tmp_str = dartResult.replace('#', '')
-    tmp_str = tmp_str.replace('*', '')
-    for i in zone:
-        tmp_str = tmp_str.replace(i, ' ')
-    tmp_li = tmp_str.split()
-
-    result = cal_number(tmp_li[0], zone_opt[0])
-    for i in range(1, len(tmp_li)):
-        bef = cal_number(tmp_li[i-1], zone_opt[i-1])
-        number = cal_number(tmp_li[i], zone_opt[i])
-        if zone_opt[i][1] == '*':
-            result += (number + bef)
-        else:
-            result += number
-
-    return result
-
-def cal_number(number, zone_opt):
-    zone = ['S', 'D', 'T']
-    if zone_opt[1] == '*':
-        return (int(number)**(zone.index(zone_opt[0]) + 1)) * 2
-    elif zone_opt[1] == '#':
-        return -int(number)**(zone.index(zone_opt[0]) + 1)
-    else:
-        return int(number)**(zone.index(zone_opt[0]) + 1)
+    return sum(scores)

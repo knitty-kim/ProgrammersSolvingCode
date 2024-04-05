8826e1ef96a5dd57b67a18740e057d1efc00cffd
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-04-05 11:31:57 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,13 +1,13 @@
 def solution(n, lost, reserve):
-    real_reser = [r for r in reserve if r not in lost]
-    real_lost = [l for l in lost if l not in reserve]
-    real_reser.sort()
-    real_lost.sort()
-    for r in real_reser:
-        f = r - 1
-        b = r + 1
-        if f in real_lost:
-            real_lost.remove(f)
-        elif b in real_lost:
-            real_lost.remove(b)
-    return n - len(real_lost)
+    reserved = 0
+    lost_li = list(set(lost) - set(reserve))
+    rsve_li = list(set(reserve) - set(lost))
+    lost_li.sort()
+    rsve_li.sort()
+    for i in lost_li:
+        for x in range(i-1, i+2):
+            if x in rsve_li:
+                rsve_li.remove(x)
+                reserved += 1
+                break
+    return n + reserved - len(lost_li)

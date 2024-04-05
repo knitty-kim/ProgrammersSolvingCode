d48f93482fa36bbc28f87932951bf2019cb14aa1
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-04-05 11:20:04 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,18 +1,13 @@
 def solution(n, lost, reserve):
-    dic = {i: 1 for i in range(1, n+1)}
-    for i in reserve:
-        dic[i] += 1
-    for i in lost:
-        dic[i] -= 1
-
-    for key in dic.keys():
-        if dic[key] == 2 and key-1 in dic:
-            if dic[key-1] == 0:
-                dic[key] -= 1
-                dic[key-1] += 1
-        if dic[key] == 2 and key+1 in dic:
-            if dic[key+1] == 0:
-                dic[key] -= 1
-                dic[key+1] += 1
-                
-    return sum(1 for i in dic.values() if i > 0)
+    real_reser = [r for r in reserve if r not in lost]
+    real_lost = [l for l in lost if l not in reserve]
+    real_reser.sort()
+    real_lost.sort()
+    for r in real_reser:
+        f = r - 1
+        b = r + 1
+        if f in real_lost:
+            real_lost.remove(f)
+        elif b in real_lost:
+            real_lost.remove(b)
+    return n - len(real_lost)

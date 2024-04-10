db24d0fb7c5d9b054d9d946c45aa3048b25c18b9
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-04-11 00:21:01 +0900

    [level 2] Title: 전화번호 목록, Time: 80.37 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,6 @@
 def solution(phone_book):
-    se = set(phone_book)
-    for phone_number in phone_book:
-        temp = ""
-        for number in phone_number:
-            temp += number
-            if temp in se and temp != phone_number:
-                return False
+    phone_book = sorted(phone_book)
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
     return True

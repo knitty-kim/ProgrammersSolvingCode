17f4af308f072e1659b08b54ec998f8d44edba59
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-04-11 22:03:49 +0900

    [level 3] Title: 베스트앨범, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -4,16 +4,13 @@ def solution(genres, plays):
     for i in range(len(plays)):
         dic[genres[i]].append(plays[i])
 
-    li = sorted(dic, key=lambda x: -sum(dic[x]))
-
-    temp_li = list((a, b) for a, b in zip(genres, plays))
+    zip_li = list((a, b) for a, b in zip(genres, plays))
 
     answer = []
-    for i in li:
-        li2 = sorted(dic[i], reverse=True)[:2]
-        for j in li2:
-            if (i, j) in temp_li:
-                answer.append(temp_li.index((i, j)))
-                temp_li[temp_li.index((i, j))] = -1
-                
+    for i in sorted(dic, key=lambda x: -sum(dic[x])):
+        for j in sorted(dic[i], reverse=True)[:2]:
+            if (i, j) in zip_li:
+                answer.append(zip_li.index((i, j)))
+                zip_li[zip_li.index((i, j))] = -1
+
     return answer

ef22348dfaf75048281e719a57c47a0542d65219
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-04-17 22:24:14 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -7,21 +7,18 @@ def solution(bridge_length, weight, truck_weights):
     while len(done) < len(truck_weights):
         # 경과시간 처리
         sec += 1
-
-        # 다리 위 트럭들 에이징 처리
-        if bridge:
-            for i in bridge:
-                i[1] += 1
-
+        
         if len(bridge) <= bridge_length:
+            if bridge:
+            # 다리 위 트럭들 에이징 처리
+                for i in bridge:
+                    i[1] += 1
+                if bridge[0][1] == bridge_length:
+                    done.append(bridge.pop(0))
             # 대기 트럭 한 대 뽑기
             if aging_truck:
                 temp = aging_truck[0]
-            if bridge and bridge[0][1] == bridge_length:
-                done.append(bridge.pop(0))
-            if sum(i[0] for i in bridge) + temp[0] <= weight:
-                if aging_truck:
+                if sum(i[0] for i in bridge) + temp[0] <= weight:
                     bridge.append(aging_truck.pop(0))
-
 
     return sec

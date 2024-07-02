fa6c09b395a02c013a9b21621bf9f16e164f555d
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-07-02 21:10:41 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,41 +1,29 @@
 # 연산자 끼워 넣기
-# from itertools import product
-# n = 4
-# print(list(product(['+', '-', '*', '/'], repeat=(n-1))))
 
-# 0. 입력 받기
 n = int(input())
 data = list(map(int, input().split()))
 add, sub, mul, div = map(int, input().split())
 
-min_value = 1e9
-max_value = -1e9
+min_val = 1e9
+max_val = -1e9
 
-def dfs(i, now):
-    global min_value, max_value, add, sub, mul, div
-    # 모든 연산자를 다 사용한 경우, 최소값과 최대값 업데이트
+def recursive(total, i, add, sub, mul, div):
+    global max_val, min_val, data, n
     if i == n:
-        min_value = min(min_value, now)
-        max_value = max(max_value, now)
+        max_val = max(max_val, total)
+        min_val = min(min_val, total)
+        return
     else:
-        if add > 0:
-            add -= 1
-            dfs(i+1, now + data[i])
-            add += 1
-        if sub > 0:
-            sub -= 1
-            dfs(i+1, now - data[i])
-            sub += 1
-        if mul > 0:
-            mul -= 1
-            dfs(i+1, now * data[i])
-            mul += 1
-        if div > 0:
-            div -= 1
-            dfs(i+1, int(now / data[i]))
-            div += 1
+        if add != 0:
+            recursive(total + data[i], i+1, add-1, sub, mul, div)
+        if sub != 0:
+            recursive(total - data[i], i+1, add, sub-1, mul, div)
+        if mul != 0:
+            recursive(total * data[i], i+1, add, sub, mul-1, div)
+        if div != 0:
+            recursive(int(total / data[i]), i+1, add, sub, mul, div-1)
 
-dfs(1, data[0])
+recursive(data[0], 1, add, sub, mul, div)
 
-print(int(max_value))
-print(int(min_value))
+print(int(max_val))
+print(int(min_val))

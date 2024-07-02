7fa65d2b781a1f6532da9478ca92f8a486a1b06f
Kitae Kim <114971312+knitty-kim@users.noreply.github.com>
2024-07-02 17:46:03 +0900

    [level 2] Title: 괄호 변환, Time: 0.13 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -1,51 +1,53 @@
 # 괄호 변환
-from collections import deque
-# 3. u 올바름 판단
-def correct(u):
-    if u[0] == '(':
-        return True
-    else:
-        return False
+
+# 정확한 구현, 재귀 함수 구현 필요
+# 코드 단순화 필요
+
+# "균형잡힌 문자열"의 인덱스 반환
+def balanced_index(p):
+    count = 0
+    for i in range(len(p)):
+        if p[i] == '(':
+            count += 1
+        else:
+            count -= 1
+        if count == 0:
+            return i
+
+# "올바른 문자열" 판단
+def check_correct(p):
+    count = 0
+    for i in p:
+        if i == '(':
+            count += 1
+        else:
+            if count == 0:
+                return False
+            count -= 1
+    return True
 
 def solution(p):
-    count_l = 0
-    count_r = 0
-    queue = deque(list(p))
-    # print(queue)
-    u = ''
-    v = ''
-    while queue:
-        c = queue.popleft()
-        # print("뽑은 값 : " + c)
-        # 1. 입력이 빈 문자열..
-        if c == "":
-            return ""
-        # 2. u, v로 분리
-        if c == '(':
-            count_l += 1
-            u += c
-        else:
-            count_r += 1
-            u += c
+    answer = ''
 
-        if count_l == count_r:
-            while queue:
-                v += queue.popleft()
-            # print("u값 : " + u + ", v값 : " + v)
+    # 1. 빈 문자열 반환
+    if p == '':
+        return answer
 
-            if correct(u):
-                u += solution(v)
+    # 2. u, v 분리
+    idx = balanced_index(p)
+    u = p[:idx+1]
+    v = p[idx+1:]
+
+    if check_correct(u):
+        answer = u + solution(v)
+    else:
+        answer = "(" + solution(v) + ")"
+        u = list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
             else:
-                temp = "(" + solution(v) + ")"
-                for i in u[1:len(u)-1]:
-                    if i == '(':
-                        temp += ')'
-                    else:
-                        temp += '('
-                return temp
+                u[i] = '('
+        answer += ''.join(u)
 
-    return u
-
-
-
-
+    return answer
